12fb24a1528cb4078c382bd2df9e468257f50026
Vavi_sq66 <Vavi_sq66@student.itstep.org>
2022-02-11 22:20:54 +0200

    new

---
+++ Text.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace CountText
+{
+    partial class Text
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Text));
+            this.progressBarText = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDownlooad = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // progressBarText
+            // 
+            this.progressBarText.Location = new System.Drawing.Point(41, 274);
+            this.progressBarText.Maximum = 40;
+            this.progressBarText.Name = "progressBarText";
+            this.progressBarText.Size = new System.Drawing.Size(697, 29);
+            this.progressBarText.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(41, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 38);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Текст";
+            // 
+            // btnDownlooad
+            // 
+            this.btnDownlooad.Font = new System.Drawing.Font("Palatino Linotype", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDownlooad.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnDownlooad.Location = new System.Drawing.Point(222, 341);
+            this.btnDownlooad.Name = "btnDownlooad";
+            this.btnDownlooad.Size = new System.Drawing.Size(380, 46);
+            this.btnDownlooad.TabIndex = 3;
+            this.btnDownlooad.Text = "Загрузить";
+            this.btnDownlooad.UseVisualStyleBackColor = true;
+            this.btnDownlooad.Click += new System.EventHandler(this.btnDownlooad_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(41, 68);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(697, 187);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // Text
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnDownlooad);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBarText);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Text";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Text";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBarText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDownlooad;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
---
+++ Text.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CountText
+{
+//Задание 1
+//Написать приложение, которое отображает количество текста, прочитанного из файла с помощью ProgressBar.
+    public partial class Text : Form
+    {
+        public Text()
+        {            
+            InitializeComponent();
+            this.Text = "Количество текста, прочитанного из файла";
+        }
+
+        string fileName;//название файла
+
+        private void btnDownlooad_Click(object sender, EventArgs e)
+        {
+            var f = new OpenFileDialog();
+            f.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            f.FileName = fileName;
+            // 1. Открытие окна и проверка, выбран ли файл
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                // 2. Вывести имя файла на форме в компоненте label1
+                label1.Text = "Открыт файл : " + f.FileName;
+                //// 3. Прочитать файл в richTextBox1
+                //// очистить предыдущий текст в richTextBox1
+                richTextBox1.Clear();
+                // 4. Создать объект класса StreamReader и прочитать данные из файла
+                StreamReader sr = File.OpenText(f.FileName);
+                // дополнительная переменная для чтения строки из файла
+                string line = null;
+                //перемення для progressBar
+                int i = 0;
+                line = sr.ReadLine(); // чтение первой строки
+                // 5. Цикл чтения строк из файла, если строки уже нет, то line=null
+                while (line != null)
+                {
+                    // 5.1. Добавить строку в richTextBox1
+                    richTextBox1.AppendText(line);
+                    // 5.2. Добавить символ перевода строки
+                    richTextBox1.AppendText("\r\n");
+                    // 5.3. Считать следующую строку
+                    line = sr.ReadLine();
+                    i++;//увеличить счетчик 
+                    progressBarText.Value = i;//вывести в progressBar
+                }
+                // 6. Закрыть соединение с файлом
+                sr.Close();
+            }
+        }
+    }
+}
+
